d611a4fb78bb0d7196c516f1040dba14c01615dc
gokulakrishnaa <gokulakrishnaapsivam@gmail.com>
2021-12-12 16:40:15 +0530

    activate

--- src/ActivateUser.js
+++ src/ActivateUser.js
@@ -1,13 +1,10 @@
 import "./ActivateUser.css";
 import React from "react";
-import { useParams } from "react-router-dom";
 import Button from "@mui/material/Button";
 import { useHistory } from "react-router-dom";
 
 export function ActivateUser() {
   const history = useHistory();
-  // eslint-disable-next-line
-  const { token } = useParams();
   return (
     <div className="activate-container">
       <h1>Account Activation</h1>
